1ba84f73aff42f60dfc7b2597be4aa2799593ce7
Student <example@example.com>
2019-08-29 16:47:44 -0700

    learn gamechoice

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+  $("#cg").click(function(){
+change();
+  })
 });
 
 
@@ -6,9 +9,35 @@ $(document).ready(function(){
 function rollDice() {
 var play1=Math.floor(Math.random() * 6) +1;
 var play2=Math.floor(Math.random() * 6) +1;
+var play3=Math.floor(Math.random() * 6) +1;
 var dicetotal= play1+play2;
 
 $("#dice1").text(play1)
-$("#dice2").text(play2);
+$("#dice2").text(play2)
+$("#dice3").text(play3)
+}
+
+var play1=1;
+var play2=2;
+var play3=3;
+
+
+var currentplay = play1;
+
+var change= function() {
+alert("change");
+if (currentplay===play1){
+  currentplay=play2
+  }
+  if (currentplay===play2){
+    currentplay=play3
+  }
+  else {
+    currentplay =play1
+    }
+
+
+
+console.log(currentplay);
 
 }
